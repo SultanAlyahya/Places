23c194f2a61f42662f06c80bcaaafc84150fa8aa
SultanAlyahya <38963523+SultanAlyahya@users.noreply.github.com>
2020-07-09 01:44:26 +0300

    first

---
+++ app.py
@@ -0,0 +1,81 @@
+from flask import Flask, jsonify, request, abort
+from models import setup_db, Togo, Went
+from auth import requires_auth
+
+app = Flask(__name__)
+setup_db(app)
+
+@app.route('/', methods=['GET'])
+def test():
+    return "works"
+
+@app.route('/togo', methods=['GET'])
+@requires_auth('get:location')
+def all(jwt):
+    togos = Togo.query.all()
+    formatted_togo = [togo.format() for togo in togos]
+    print(formatted_togo)
+    return jsonify({'togos': formatted_togo})
+
+@app.route('/togo', methods=['POST'])
+@requires_auth('post:location')
+def add_togo(jwt):
+    try:
+        Togo( location=request.get_json()['location'],
+              date=request.get_json()['date'],
+              description=request.get_json()['description']).insert()
+        return jsonify({'done': 'yes'}), 201
+    except Exception:
+        return jsonify({'done': 'no'}), 500
+
+@app.route('/togo/<id>', methods=['PATCH'])
+@requires_auth('patch:location')
+def edit_togp(jwt, id):
+    try:
+        togo = Togo.query.get(id)
+        if not togo:
+            return abort(404)
+        if 'location' in request.get_json():
+            togo.location = request.get_json()['location']
+
+        if 'date' in request.get_json():
+            togo.date = request.get_json()['date']
+
+        if 'description' in request.get_json():
+            togo.description = request.get_json()['description']
+
+        togo.update()
+        print(togo)
+        return jsonify({'done': 'yes'}), 200
+    except Exception:
+        return jsonify({'done': 'no'}), 500
+
+@app.route('/togo/<id>', methods=['DELETE'])
+@requires_auth('delete:location')
+def delete_togo(jwt, id):
+    try:
+        togo = togo = Togo.query.get(id)
+        if not togo:
+            return jsonify({'done': 'no'}), 404
+        togo.delete()
+        return jsonify({'done': 'yes'}), 200
+    except Exception:
+        return jsonify({'done': 'no'}), 500
+
+@app.route('/went', methods=['GET'])
+@requires_auth('get:went')
+def went(jwt):
+    wents = Went.query.all()
+    formatted_went = [went.format() for went in wents]
+    print(formatted_went)
+    return jsonify({'went': formatted_went})
+   
+@app.route('/went', methods=['POST'])
+@requires_auth('post:went')
+def add_went(jwt):
+    try:
+        Went( location=request.get_json()['location'],
+              description=request.get_json()['description']).insert()
+        return jsonify({'done': 'yes'}), 201
+    except Exception:
+        return jsonify({'done': 'no'}), 500
---
+++ models.py
@@ -0,0 +1,66 @@
+from sqlalchemy import Column, String, Integer, create_engine
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_path = "postgres://postgres:14231423az@localhost:5432/places"
+
+db = SQLAlchemy()
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+class Togo(db.Model):
+    __tablename__ = "togo"
+
+    id = Column(Integer, primary_key=True)
+    location = Column(String)
+    date = Column(String)
+    description = Column(String)
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return{
+            'id': self.id,
+            'location': self.location,
+            'date': self.date,
+            'description': self.description
+        }
+
+class Went(db.Model):
+    __tablename__ = "went"
+
+    id = Column(Integer, primary_key=True)
+    location = Column(String)
+    description = Column(String)
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+  
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return{
+            'id': self.id,
+            'location': self.location,
+            'description': self.description
+        }
